63a8a1e1cbb88ca77828ea84b628fc6a5e2f43e4
Javier Cuadrado <mantecajc@gmail.com>
2021-12-15 16:16:22 +0100

    Included links on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <p>If you don't know Docusaurus...</p>
+        <p>If you don't know yet Docusaurus...</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
