9eff9515a95136821e57fdd867698aedad5407cf
Javier Cuadrado <mantecajc@gmail.com>
2022-07-04 11:27:07 +0200

    feat: add search-local feature
     - source: https://github.com/cmfcmf/docusaurus-search-local
     - last update: 22 days ago
     - to test locally:
    	yarn add @cmfcmf/docusaurus-search-local
    	yarn build
    	npm run serve

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,8 @@ const config = {
     ],
   ],
 
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
